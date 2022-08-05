4035b5fc0333e7a2ac750a43421a36c554a0548b
Thenia <theniakoutrouli@gmail.com>
2022-08-05 19:07:38 +0200

    added BSON serialization attribute

--- FacebookCloneAPI/Models/User.cs
+++ FacebookCloneAPI/Models/User.cs
@@ -1,16 +1,22 @@
-﻿using System;
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
 namespace FacebookCloneAPI.Models
 {
     public class User
     {
+        [BsonElement]
         public Guid Id { get; set; }
 
+        [BsonElement]
         public string Username { get; set; }
 
+        [BsonElement]
         public string Password { get; set; }
 
+        [BsonElement]
         public string FirstName { get; set; }
 
+        [BsonElement]
         public string LastName { get; set; }
     }
 }
