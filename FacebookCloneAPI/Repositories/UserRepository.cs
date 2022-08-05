1beb69d0024621b31e1fb72ef7ae373344392ded
Thenia <theniakoutrouli@gmail.com>
2022-08-05 16:25:18 +0200

    Added User db connection

--- FacebookCloneAPI/Repositories/UserRepository.cs
+++ FacebookCloneAPI/Repositories/UserRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace FacebookCloneAPI.Repositories
+﻿using FacebookCloneAPI.Models;
+using MongoDB.Driver;
+
+namespace FacebookCloneAPI.Repositories
 {
     public class UserRepository : IUserRepository
     {
+        private readonly IMongoCollection<User> db;
+
+        public UserRepository(IMongoClient client)
+        {
+            var database = client.GetDatabase("Facebook");
+            db = database.GetCollection<User>("Users");
+        }
     }
 }
